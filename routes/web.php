14328132f749d68938af7998f1ab0dfa6508134a
Godbless Nyagawa <godblessnyagawa@gmail.com>
2026-01-07 08:05:53 +0300

    feat: views

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+Route::get('/add', function () {
+    return view('add');
+});
+// http://localhost:5173/
